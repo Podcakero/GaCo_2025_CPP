4bcd5afb28b1f9265aee141b431b0a34848bd83a
Phil Malone <phil.malone@mr-phil.com>
2025-02-03 11:01:18 -0500

    Tested PathPlanner Integration.

--- src/main/java/frc/robot/subsystems/Tower.java
+++ src/main/java/frc/robot/subsystems/Tower.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import com.pathplanner.lib.config.PIDConstants;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -35,7 +37,6 @@ public class Tower extends SubsystemBase {
 
   private PIDController finController;
   private PIDController liftController;
-
 
   @Override
   public void periodic() {
