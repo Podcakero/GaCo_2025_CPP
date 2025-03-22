cd89ae63802c9a2fa923cb6877c42b6eba1a77f3
GaCo 1629 <59513563+GaCo1629@users.noreply.github.com>
2025-03-22 10:01:33 -0400

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -97,7 +97,7 @@ public class Constants {
         public static final Distance kL2AlgaeHeight = Inches.of(39);
         public static final Distance kL3AlgaeHeight = Inches.of(54);  
 
-        public static final Distance kL4AlgaeWindupHeight = Inches.of(71.5);  
+        public static final Distance kL4AlgaeWindupHeight = Inches.of(70.5);  
 
         public static final Distance kSafeHomeHeight = Inches.of(19);
         
